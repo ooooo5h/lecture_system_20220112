1dfaf2e6df0d32ac1d1f6a325c572107a0929aa3
ooooo5h <ooooo5h91@gmail.com>
2022-01-12 17:00:24 +0900

    로그인 함수 실행
    - request import -> args를 dict()형태로 넘겨주자

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 # 플라스크 자체를 로딩
 
-from flask import Flask
+from flask import Flask, request
 
 from server.db_connector import DBConnector
 
-from .api.user import test
+from .api.user import login
 from .api.lecture import lecture_test
 
 # DB연결 정보를 관리하는 클래스 생성해서, 객체를 변수에 담아두자
@@ -16,7 +16,7 @@ def create_app():
     # 기본 로그인 기능 주소 열어주기
     @app.post("/user")
     def user_post():
-        pass
+        return login(request.args.to_dict())
 
 
     @app.post("/lecture")
