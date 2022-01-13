f159f0c394fce84c6e0d76536e2595886dadf4a5
ooooo5h <ooooo5h91@gmail.com>
2022-01-13 17:46:31 +0900

    모든 리뷰 내역을 가져오는 쿼리 추가로 작성
    - Reviews 모델 클래스 추가함

--- server/api/lecture/lecture.py
+++ server/api/lecture/lecture.py
@@ -1,6 +1,7 @@
 from server.api import lecture
 from server.model import Lectures
 from server import db
+from server.model.reviews import Reviews
 
 
 # 모든 강의 목록 <이름순으로> 내려주기
@@ -83,14 +84,23 @@ def view_lecture_detail(id, params):
     lecture = Lectures(lecture_data)
     
     
-    # 2. 강의의 평점을 추가로 조회 (해당 강의의 모든 리뷰의 점수 -> 평균)
-    # 3. 모든 리뷰 내역을 추가로 첨부.
+    # 2. 모든 리뷰 내역을 추가로 첨부.
+    sql = f"SELECT * FROM lecture_review WHERE id = {id}"
+    
+    review_data_list = db.executeAll(sql)
+    reviews = [Reviews(row).get_data_object() for row in review_data_list]
+    
+    
+    
+    # 3. 강의의 평점을 추가로 조회 (해당 강의의 모든 리뷰의 점수 -> 평균)
+
     
     
     return{
         'code' : 200,
         'message' : '강의 상세 조회',
         'data' : {
-            'lecture' : lecture.get_data_object()
+            'lecture' : lecture.get_data_object(),
+            'reviews' : reviews
         }
     }
