c7edaeb83c448085ad3047f1307f8bef9b455863
ooooo5h <ooooo5h91@gmail.com>
2022-01-13 12:48:06 +0900

    실제 강의 목록 전부 조회 기능

--- server/api/lecture/lecture.py
+++ server/api/lecture/lecture.py
@@ -2,8 +2,19 @@ from server.model import Lectures
 from server import db
 
 
-# 모든 강의 목록 내려주기
+# 모든 강의 목록 <이름순으로> 내려주기
 def get_all_lectures(params):
+    
+    sql = "SELECT * FROM lectures ORDER BY name"
+    
+    lecture_list = db.executeAll(sql)
+    
+    lectures = [Lectures(row).get_data_object()  for row in lecture_list]
+    
     return {
-        'test' : 'test',
+        'code' : 200,
+        'message' : '모든 강의 목록 이름순으로 정렬 및 불러오기',
+        'data' : {
+            'lectures' : lectures,
+        }
     }
