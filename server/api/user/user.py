60a0d8ca3da1519da7f637e352031c6148bb17b3
ooooo5h <ooooo5h91@gmail.com>
2022-01-13 12:42:17 +0900

    user.py에서도 db변수 import 활용

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -1,10 +1,7 @@
 # 로그인 / 회원가입 등 실제 사용자 정보 관련된 기능들을 모아두는 모듈
 # DB연결정보를 보관한 변수를 import하면 쉽게 쓰겠지?
-from server.db_connector import DBConnector
 from server.model import Users
-
-db = DBConnector()
-
+from server import db
 
 # 로그인 기능
 def login(params):
