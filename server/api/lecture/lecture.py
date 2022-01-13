3365bba52ef0d7f647f889a2f61b245dae811f6b
ooooo5h <ooooo5h91@gmail.com>
2022-01-13 10:15:38 +0900

    실제 강의 목록 전부 조회하는 기능 생성

--- server/api/lecture/lecture.py
+++ server/api/lecture/lecture.py
@@ -2,7 +2,18 @@ from server.model import Lectures
 from server import db
 
 def get_all_lecture(params):
-    # 모든 강의 목록을 내려주자 => 당장은 params 변수 활용 X
+    # 모든 강의 목록을 <이름순으로> 내려주자 => 당장은 params 변수 활용 X
+    
+    sql = f"SELECT * FROM lectures ORDER BY name"
+    
+    db_list = db.executeAll(sql)
+    
+    lectures = [ Lectures(row).get_data_object() for row in db_list ]
+    
     return {
-        '임시' : '또임시'
+        'code' : 200,
+        'message' : '모든 강의 목록 조회',
+        'data' : {
+            'lectures' : lectures,
+        }
     }
